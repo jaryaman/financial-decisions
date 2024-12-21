28999a3dea16117b9ff36c02ef8544a5765c782d
jaryaman <j.aryaman25@gmail.com>
2024-12-21 09:45:55 +0000

    ok

---
+++ src/findec/__init__.py
@@ -0,0 +1,3 @@
+from findec.utility import crra_utility, certainty_equivalent_return
+
+__all__ = ["crra_utility", "certainty_equivalent_return"]
---
+++ src/findec/utility.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def crra_utility(w: np.ndarray | float, *, gamma: float):
+    """
+    Interestingly, also known as the Box-Cox transformation in stats
+    https://en.wikipedia.org/wiki/Isoelastic_utility
+    """
+    if gamma == 1:
+        return np.log(w)
+    return (1 - w ** (1 - gamma)) / (gamma - 1)
+
+
+def certainty_equivalent_return(
+    *, initial_wealth: float, expected_utility: float, gamma: float
+):
+    if gamma == 1.0:
+        certainty_equivalent_final_wealth = np.exp(expected_utility)
+    else:
+        certainty_equivalent_final_wealth = (1 - (gamma - 1) * expected_utility) ** (
+            1 / (1 - gamma)
+        )
+    certainty_equivalent_return = certainty_equivalent_final_wealth / initial_wealth - 1
+    return certainty_equivalent_return
