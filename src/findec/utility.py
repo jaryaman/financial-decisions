d2b437b339370a36ecea6ddb5848216518b94ff6
jaryaman <j.aryaman25@gmail.com>
2024-12-21 17:42:20 +0000

    got chatGPT to do the whole damn thing

--- src/findec/utility.py
+++ src/findec/utility.py
@@ -1,11 +1,13 @@
 import numpy as np
 
 
-def crra_utility(w: np.ndarray | float, *, gamma: float):
+def crra_utility(w: np.ndarray | float, *, gamma: float, eps: float = 1e-8):
     """
     Interestingly, also known as the Box-Cox transformation in stats
     https://en.wikipedia.org/wiki/Isoelastic_utility
     """
+    if w < eps:
+        return -1e9
     if gamma == 1:
         return np.log(w)
     return (1 - w ** (1 - gamma)) / (gamma - 1)
